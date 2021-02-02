58e3ef3fb8c0ddb65dc75613b44d89870eead9d0
yco1g18 <yco1g18@soton.ac.uk>
2021-02-02 14:31:39 +0000

    Add sign up functionality

--- android-frontend/app/src/main/java/com/example/touchauthenticator/AuthActivity.kt
+++ android-frontend/app/src/main/java/com/example/touchauthenticator/AuthActivity.kt
@@ -60,23 +60,24 @@ class AuthActivity:AppCompatActivity() {
             }
     }
 
-    fun signup(email:String, password:String) {
+    fun signUp(view:View) {
+        email = findViewById<EditText>(R.id.username).text.toString() + "@test.com"
+        password = findViewById<EditText>(R.id.password).text.toString()
         auth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
                     // Sign in success, update UI with the signed-in user's information
                     Log.d(TAG, "createUserWithEmail:success")
+                    Toast.makeText(baseContext, "Sign Up Success!",
+                        Toast.LENGTH_SHORT).show()
                     val user = auth.currentUser
                 } else {
                     // If sign in fails, display a message to the user.
                     Log.w(TAG, "createUserWithEmail:failure", task.exception)
-                    Toast.makeText(baseContext, "Authentication failed.",
+                    Toast.makeText(baseContext, "Sign Up.",
                         Toast.LENGTH_SHORT).show()
                 }
-
                 // ...
             }
     }
-
-
 }
