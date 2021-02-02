2444859042675256db7edb177d20f13cd27ae4aa
yco1g18 <yco1g18@soton.ac.uk>
2021-02-02 14:16:24 +0000

    add login logic

--- android-frontend/app/src/main/java/com/example/touchauthenticator/AuthActivity.kt
+++ android-frontend/app/src/main/java/com/example/touchauthenticator/AuthActivity.kt
@@ -1,10 +1,12 @@
 package com.example.touchauthenticator
 
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatEditText
 import com.google.firebase.auth.FirebaseAuth
 
 class AuthActivity:AppCompatActivity() {
@@ -12,31 +14,39 @@ class AuthActivity:AppCompatActivity() {
     private val RC_SIGN_IN = 5783
     private val TAG = "Auth Status: "
     private lateinit var auth: FirebaseAuth
+    private lateinit var email: String
+    private lateinit var password: String
 
-    /*
+
     override fun onStart() {
         super.onStart()
-        // Check if user is signed in (non-null) and update UI accordingly
         auth = FirebaseAuth.getInstance()
+        // Check if user is signed in (non-null) and update UI accordingly
+        /*
         val currentUser = auth.currentUser
         if(currentUser != null){
             startActivity(Intent(this, MainActivity::class.java))
             this.finish()
         }
+         */
     }
-     */
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
     }
 
-    fun login(email:String, password:String) {
+    fun login(view:View) {
+        email = findViewById<EditText>(R.id.username).text.toString() + "@test.com"
+        password = findViewById<EditText>(R.id.password).text.toString()
         auth.signInWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
                     // Sign in success, update UI with the signed-in user's information
                     Log.d(TAG, "signInWithEmail:success")
+                    Toast.makeText(baseContext, "Authentication success.",
+                        Toast.LENGTH_SHORT).show()
                     val user = auth.currentUser
                 } else {
                     // If sign in fails, display a message to the user.
@@ -67,4 +77,6 @@ class AuthActivity:AppCompatActivity() {
                 // ...
             }
     }
+
+
 }
