073999c44b47e7b6942eae69d529e1e1aaa1d11d
yco1g18 <yco1g18@soton.ac.uk>
2021-02-02 16:09:04 +0000

    add homepage

--- android-frontend/app/src/main/java/com/example/touchauthenticator/HomeActivity.kt
+++ android-frontend/app/src/main/java/com/example/touchauthenticator/HomeActivity.kt
@@ -1,4 +1,26 @@
 package com.example.touchauthenticator
 
-class HomeActivity {
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class HomeActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+
+    fun launchTestingUI(view: View) {
+
+    }
+
+    fun launchEnrolmentUI(view: View) {
+
+    }
+
+    fun launchStatisticsUI(view: View) {
+
+    }
+
 }
