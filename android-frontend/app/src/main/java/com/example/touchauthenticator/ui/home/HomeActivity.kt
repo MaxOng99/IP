30fed3369f3fbb60275557d961b8560476713850
yco1g18 <yco1g18@soton.ac.uk>
2021-02-07 16:37:21 +0000

    refactor HomeActivity to use ActivityLauncher utility

--- android-frontend/app/src/main/java/com/example/touchauthenticator/ui/home/HomeActivity.kt
+++ android-frontend/app/src/main/java/com/example/touchauthenticator/ui/home/HomeActivity.kt
@@ -6,6 +6,7 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.touchauthenticator.R
 import com.example.touchauthenticator.ui.enrolment.EnrolmentActivity
+import com.example.touchauthenticator.utility.ActivityLauncher
 import com.google.firebase.auth.FirebaseUser
 
 class HomeActivity: AppCompatActivity() {
@@ -19,17 +20,15 @@ class HomeActivity: AppCompatActivity() {
     }
 
     fun launchEnrolmentUI(view: View) {
-        val intent = Intent(this, EnrolmentActivity::class.java)
-        intent.putExtra("user", viewModel.currentUser)
-        startActivity(intent)
+        ActivityLauncher.launchReactionActivity(this, viewModel.currentUser)
     }
 
     fun launchTestingUI(view: View) {
-
+        ActivityLauncher.launchTestingActivity()
     }
 
     fun launchStatisticsUI(view: View) {
-
+        ActivityLauncher.launchTestingActivity()
     }
 
 }
