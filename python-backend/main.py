8f28f1aaaff9eb95f555458966f81f58e1dd5bd8
yco1g18 <yco1g18@soton.ac.uk>
2021-03-09 01:40:41 +0000

    add server side configuration.

--- python-backend/main.py
+++ python-backend/main.py
@@ -1,10 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Header
+from firebase_amdin import auth
+
 
 app = FastAPI()
 
 @app.get('/')
 def index():
 	return {'key': 'value'}
+
+@app.get('/auth')
+def authenticate(*, jwt_token = Header(None)):
+	decoded_token = auth.verify_id_token(jwt_token )
+	uid = decoded_token['uid']
+	return {"Authorization": jwt_token}
 
 
 
