f9371691c3ac7737f30d9672d89c5c804aaf3859
max_gcp <ongyicheng99@gmail.com>
2021-03-09 12:09:22 +0000

    removed firebase auth code for debuggin purposes.

--- python-backend/main.py
+++ python-backend/main.py
@@ -1,18 +1,7 @@
-from fastapi import FastAPI, Header
-from firebase_admin  import auth
-
+from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get('/')
 def index():
 	return {'key': 'value'}
-
-@app.get('/auth')
-def authenticate(*, jwt_token = Header(None)):
-	decoded_token = auth.verify_id_token(jwt_token )
-	uid = decoded_token['uid']
-	return {"Authorization": jwt_token}
-
-
-
