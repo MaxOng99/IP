e69475bdd2d0f251e9c968c069806317c5b70a7e
yco1g18 <yco1g18@soton.ac.uk>
2021-03-09 18:29:29 +0000

    init firebase-admin-sdk

--- python-backend/main.py
+++ python-backend/main.py
@@ -1,7 +1,22 @@
-from fastapi import FastAPI
+import firebase_admin
+from firebase_admin import auth
+from firebase_admin import credentials
+from fastapi import FastAPI, Header
+from firebase_admin.auth import InvalidIdTokenError
 
 app = FastAPI()
+admin_sdk_path = "C:/Users/Lenovo/google/ta_proj_admin_sdk_service_account_file.json"
+cred = credentials.Certificate(admin_sdk_path)
+firebase_admin.initialize_app(cred)
 
 @app.get('/')
 def index():
 	return {'key': 'value'}
+
+@app.get('/auth')
+def authenticate(*, jwt_token: str = Header(None)):
+	try:
+		decoded_token = auth.verify_id_token(jwt_token)
+		return {"uid": decoded_token['uid']}
+	except InvalidIdTokenError as ex:
+		return {'error': ex}
