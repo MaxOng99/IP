129087abe28fdf61d4ca9cb0ce1f7cae7198da6d
yco1g18 <yco1g18@soton.ac.uk>
2021-03-29 20:50:24 +0100

    restructured main.py. Original codebase for this file is partitioned into other modules.

--- python-backend/main.py
+++ python-backend/main.py
@@ -1,42 +1,18 @@
+# FastAPI imports
 from fastapi import FastAPI, Header
+
+# Firebase imports
 import firebase_admin
 from firebase_admin.auth import InvalidIdTokenError
-from firebase_admin import auth
-from pydantic import BaseModel
-from typing import Mapping, List
-import pickle
 
-## Global Variables
-user_model_mapping = dict()
-
-## Data Classes
-class TouchGestureData(BaseModel):
-	data: List[Mapping[str, float]]
-	date: str
-	sample_id: int
-
-class Request(BaseModel):
-	request: Mapping[int, List[TouchGestureData]]
-
-class Response(BaseModel):
-	#response: Mapping[int, int] 
-	response: str
-
-## Helper functions
-def init_models():
-	for id in range(1, 5):
-		with open(f'models/model{id}.pkl') as f:
-			user_model_mapping[id] = pickle.load(f)
-
-def authenticate(jwt_token):
-	decoded_token = auth.verify_id_token(jwt_token)
-	return decoded_token['uid']
+# Custom module imports
+from utilities import authenticate
+from utilities import get_model, prepare_data, evaluate
+from data_objects import TouchGestureData, UserTGDMapping, EvaluationResult
 
 ## App Initialisation
 default_app = firebase_admin.initialize_app()
 app = FastAPI()
-
-#init_models()
 
 ## API Routings
 @app.get('/')
@@ -44,27 +20,17 @@ def index():
 	return {'key': 'value'}
 
 @app.post('/predict')
-def predict(request: Request, jwt_token = Header(None)):
+def predict(request: UserTGDMapping, jwt_token = Header(None)):
 
 	try:
 		uid = authenticate(jwt_token)
-		#model = user_model_mapping[uid]
-		#result = dict()
-		#sresponse = dict()
 
-		"""
-		for uid, touch_gesture_data_list in request:
-			# result[uid] = [model.predict(touch_gesture.data) for touch_gesture in touch_gesture_data_list]
-
-		for uid, predictions in result:
-			true_predictions = [x if x == 1 for x in predictions]
-			response[uid] = len(true_predictions)
-
-		return response
-		"""
-		return "auth success"
+		model = get_model(uid)
+		prepared_data = prepare_data(request)
+		evaluation_object = evaluate(prepare_data, uid)
+		return evaluation_object
 
 	except InvalidIdTokenError:
-		print("Authroization failed")
+		return "Authroization failed"
 
 
