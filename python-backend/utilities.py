98709ca509bd2a1aa0b95e048ad432cc7a7a69b9
yco1g18 <yco1g18@soton.ac.uk>
2021-03-29 20:47:11 +0100

    create utilities module that contains frequently used functions.

---
+++ python-backend/utilities.py
@@ -0,0 +1,34 @@
+import pickle
+from firebase_admin import auth
+from pipeline import DataPreparer, Preprocessor, Evaluator
+
+
+def get_model(uid):
+	with open(f'models/model{id}.pkl', 'rb') as file:
+		return pickle.load(file)
+
+def authenticate(jwt_token):
+	decoded_token = auth.verify_id_token(jwt_token)
+	return decoded_token['uid']
+
+def prepare_data(users_df):
+	df = DataPreparer(users_df)
+	preprocessor = Preprocessor(df.users_df)
+	return preprocessor.df
+
+def evaluate(model, users_df, uid):
+	evaluator = Evaluator(model, users_df, uid)
+	evaluation_object = dict()
+
+	for i in range(1, 5):
+		evaluation_object[i] = evaluator.get_prediction_results(i)
+
+	far, frr, eer = evaluator.evaluate()
+	evaluation_object['far'] = far
+	evaluation_object['frr'] = frr
+	evaluation_object['eer'] = eer
+
+	return evaluation_object 
+
+
+
