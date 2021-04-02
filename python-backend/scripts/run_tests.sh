20eca5a8dc6a9750f11411c5fcba37c700bfca3b
yco1g18 <yco1g18@soton.ac.uk>
2021-04-03 00:45:14 +0100

    Pylint python files only.

--- python-backend/scripts/run_tests.sh
+++ python-backend/scripts/run_tests.sh
@@ -1,7 +1,7 @@
 cd python-backend/
 
 pip install -r scripts/requirements.txt
-python -m pylint --rcfile=scripts/.pylintrc src/main
+python -m pylint --rcfile=scripts/.pylintrc src/main/*.py
 
 cd src/tests
 python -m pytest
