3c4d65b6435aee1b6f5497c3d32bd093c1329bce
yco1g18 <yco1g18@soton.ac.uk>
2021-04-03 00:37:20 +0100

    run pylint module as script.

--- python-backend/scripts/run_tests.sh
+++ python-backend/scripts/run_tests.sh
@@ -1,7 +1,7 @@
 cd python-backend/
 
 pip install -r scripts/requirements.txt
-pylint --rcfile=scripts/.pylintrc src/main
+python -m pylint --rcfile=scripts/.pylintrc src/main
 
 cd src/tests
 python -m pytest
