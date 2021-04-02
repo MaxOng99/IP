24518902140a323fa2e92e63924740832c6c7928
yco1g18 <yco1g18@soton.ac.uk>
2021-04-02 16:10:47 +0100

    update CI files.

--- python-backend/scripts/run_tests.sh
+++ python-backend/scripts/run_tests.sh
@@ -1,5 +1,5 @@
 pip install -r python-backend/scripts/requirements.txt -t /workspace/python-backend/lib
-pip install -e src/main -t /workspace/python-backend/lib
+pip install -e -t /workspace/python-backend/lib
 
 cd python-backend/src/tests/
 python -m pytest
