21e6031c060c919e6a03f1a6656e8d594bb026a1
yco1g18 <yco1g18@soton.ac.uk>
2021-04-03 01:07:39 +0100

    run pylint_runner as a package.

--- python-backend/scripts/run_tests.sh
+++ python-backend/scripts/run_tests.sh
@@ -3,7 +3,7 @@ cd python-backend/
 pip install -r scripts/requirements.txt
 
 cd src/main
-python -m pylint_runner -rcfile=scripts/.pylintrc
+pylint_runner -rcfile=scripts/.pylintrc
 
 cd ../tests
 python -m pytest
